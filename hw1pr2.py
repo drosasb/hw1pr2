06a9493cfb5374d53fd2258673e27dcc32f12d32
drosasb <112618859+drosasb@users.noreply.github.com>
2022-09-01 15:21:48 +0300

    Add files via upload

---
+++ hw1pr2.py
@@ -0,0 +1,23 @@
+# CSCI1010: Lab 1, Problem 1
+# Filename: hw1pr2.py
+# Name: 
+# Problem description: Solving the quadratic equation!
+
+from math import *
+
+x = input("Supply a number: ")
+x = float(x)
+
+
+a = input("Supply a value for a: ")
+a = float(a)
+
+b = input("Supply a value for b: ")
+b = float(b)
+
+c = input("Supply a value for c: ")
+c = float(c)
+
+a*x**2 + b*x + c == 0
+
+print('x')
